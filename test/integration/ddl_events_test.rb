67e636d9cc926ab9765af91ea73d2bd186a75bee
Shiv Nagarajan <shiv@shopify.com>
2021-12-17 14:56:59 -0500

    Add actual ddl test
    Co-authored-by: Aaron Brady <aaron.brady@shopify.com>
    Co-authored-by: Pawan Dubey <pawan.dubey@shopify.com>

--- test/integration/ddl_events_test.rb
+++ test/integration/ddl_events_test.rb
@@ -6,12 +6,7 @@ class DdlEventsTest < GhostferryTestCase
   def test_default_event_handler
     seed_simple_database_with_single_table
 
-    #datawriter = new_source_datawriter
-
     ghostferry = new_ghostferry(MINIMAL_GHOSTFERRY)
-
-    #start_datawriter_with_ghostferry(datawriter, ghostferry)
-    #stop_datawriter_during_cutover(datawriter, ghostferry)
 
     ghostferry.run_with_logs()
 
@@ -19,12 +14,34 @@ class DdlEventsTest < GhostferryTestCase
   end
 
   def test_ddl_event_handler
-    #skip("skipping")
     seed_simple_database_with_single_table
 
     ghostferry = new_ghostferry(DDL_GHOSTFERRY)
     ghostferry.run_with_logs()
 
     assert_ghostferry_completed(ghostferry, times: 1)
+  end
+
+  def test_ddl_event_handler_with_ddl_events
+    seed_simple_database_with_single_table
+
+    table_name = full_table_name(DEFAULT_DB, DEFAULT_TABLE)
+
+    ghostferry = new_ghostferry(DDL_GHOSTFERRY)
+
+    ghostferry.on_status(GhostferryHelper::Ghostferry::Status::BINLOG_STREAMING_STARTED) do
+      source_db.query("INSERT INTO #{table_name} VALUES (9000, 'test')")
+      source_db.query("ALTER TABLE #{table_name} ADD INDEX (data(100))")
+      source_db.query("INSERT INTO #{table_name} (id, data) VALUES (9001, 'test')")
+    end
+
+    ghostferry.run_expecting_failure
+
+    source, target = source_and_target_table_metrics
+    source_count = source[DEFAULT_FULL_TABLE_NAME][:row_count]
+    target_count = target[DEFAULT_FULL_TABLE_NAME][:row_count]
+
+    refute_equal(source_count, target_count, "target should have fewer rows than source")
+
   end
 end
